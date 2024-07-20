a415feb378692997f96dc872208eb0930907fa58
Dimitris Orfanoudakis <dimitrisorfanudakis@gmail.com>
2024-07-21 01:55:54 +0300

    feat: Adds models in the admin UI

--- delty/admin.py
+++ delty/admin.py
@@ -1,1 +1,8 @@
 # Register your models here.
+from django.contrib import admin
+from delty.models import UrlAddress, PageSnapshot, SelectedElement, CrawlingJob
+
+admin.site.register(UrlAddress)
+admin.site.register(PageSnapshot)
+admin.site.register(SelectedElement)
+admin.site.register(CrawlingJob)
--- delty/urls.py
+++ delty/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from django.contrib import admin
 from delty.views.auth import register, login
 from delty.views.crawler import RenderURLView
 from delty.views.crawler import CrawlerView
 from delty.views.index import IndexView
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="index"),
     path("render_url", RenderURLView.as_view(), name="render_url"),
     path("initiate_crawilng", CrawlerView.as_view(), name="initiate_crawilng"),
