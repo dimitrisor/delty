2cb99b8c9d2ccf5a6b96705abcafadbd1a312c44
Dimitris Orfanoudakis <dimitris@taus.net>
2024-07-24 11:46:47 +0300

    fix: Fix the init-s3-bucket.sh script

--- init-s3-bucket.sh
+++ init-s3-bucket.sh
@@ -1,4 +1,7 @@
-# -- > Create S3 Bucket
-echo $(awslocal s3 mb s3://selected-element-html-s3-bucket)
-# --> List S3 Buckets
-echo $(awslocal s3 ls)
+#!/bin/bash
+
+echo "Create S3 Bucket..."
+awslocal s3 mb s3://selected-element-html-s3-bucket
+
+echo "List S3 Buckets..."
+awslocal s3 ls
