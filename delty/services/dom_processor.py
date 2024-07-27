59becd277eb1839f877e47f30d54473751256e87
Dimitris Orfanoudakis <dimitris@taus.net>
2024-07-27 10:15:09 +0300

    fix: General cleaning

--- delty/services/dom_processor.py
+++ delty/services/dom_processor.py
@@ -1,36 +1,10 @@
-import re
-from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
 
 class DomProcessor:
     @staticmethod
-    def replace_relative_links(base_url, html_content):
-        # Define a regex pattern to find relative links in src and href attributes
-        pattern = re.compile(
-            r'(<\s*(img|script|a|link)\s+[^>]*(src|href)\s*=\s*["\'])(?!http|https|ftp|//)([^"\']+)["\']',
-            re.IGNORECASE,
-        )
-
-        # Function to replace each match with the absolute URL
-        def replace_match(match):
-            # Extract the full match and the relative URL
-            match.group(0)
-            relative_url = match.group(4)
-            absolute_url = urljoin(base_url, relative_url)
-
-            # Replace the relative URL with the absolute URL
-            new_link = match.group(1) + absolute_url + '"'
-            return new_link
-
-        # Use re.sub to replace all relative links with absolute links
-        updated_html = re.sub(pattern, replace_match, html_content)
-
-        return updated_html
-
-    @staticmethod
-    def convert_relative_to_absolute(base_url, html_content):
+    def convert_relative_to_absolute(base_url: str, html_content: str) -> str:
         soup = BeautifulSoup(html_content, "html.parser")
 
         # Convert relative paths in img tags
@@ -55,7 +29,7 @@ class DomProcessor:
         return str(soup)
 
     @staticmethod
-    def escape_css_selector(css_selector):
+    def escape_css_selector(css_selector: str) -> str:
         # List of special characters to escape
         special_chars = r"@"
         # Escape each special character
