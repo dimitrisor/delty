e6c8da16bbc0df0bb716e4c3078812ca210e8729
dimitris orfanoudakis <dimitrisorfanuakis@gmail.com>
2024-08-12 09:59:21 +0300

    fix: Minor fixes

--- delty/forms/crawling_submission.py
+++ delty/forms/crawling_submission.py
@@ -1,7 +1,65 @@
+from crispy_forms.bootstrap import FieldWithButtons, StrictButton
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Field, Layout, Row, Column, HTML
 from django import forms
 from django.core.validators import URLValidator
+from django.urls import reverse
 
 
 class CrawlingSubmissionForm(forms.Form):
-    url = forms.CharField(validators=[URLValidator()])
-    element_selector = forms.CharField(label="")
+    url = forms.CharField(
+        label="",
+        initial="http://in.gr",
+        validators=[URLValidator()],
+        widget=forms.URLInput(attrs={"placeholder": "Enter a URL"}),
+    )
+    element_selector = forms.CharField(label="", required=False)
+    iframe_width = forms.IntegerField(widget=forms.HiddenInput())
+    iframe_height = forms.IntegerField(widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        super(CrawlingSubmissionForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column(
+                    FieldWithButtons(
+                        Field("url"),
+                        StrictButton(
+                            "Fetch",
+                            type="submit",
+                            id="id_fetch",
+                            css_class="btn btn-primary",
+                            formaction="",
+                        ),
+                    ),
+                    css_class="input-group",
+                ),
+            ),
+            Row(
+                Column(
+                    HTML(
+                        "<p>Please select an element in the frame below to observe every day</p>"
+                    )
+                )
+            ),
+            Row(
+                Column(
+                    FieldWithButtons(
+                        Field(
+                            "element_selector",
+                            placeholder="No element selector generated.",
+                            readonly=True,
+                        ),
+                        StrictButton(
+                            "Crawl",
+                            type="submit",
+                            id="id_crawl",
+                            css_class="btn btn-primary disabled",
+                            formaction=reverse("initiate_crawilng"),
+                        ),
+                    ),
+                    css_class="input-group",
+                ),
+            ),
+        )
