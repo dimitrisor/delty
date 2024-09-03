5475f54a53a53452448f7392f6ecabcde6157940
dimitris orfanoudakis <dimitrisorfanuakis@gmail.com>
2024-09-03 15:46:47 +0300

    fix: Tidy up env vars

--- app/settings/extras/s3.py
+++ app/settings/extras/s3.py
@@ -4,7 +4,7 @@ from app import env
 AWS_ACCESS_KEY_ID = env.get_str("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = env.get_str("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = env.get_str("AWS_STORAGE_BUCKET_NAME", "")
-AWS_S3_ENDPOINT_URL = env.get_str("AWS_S3_HOST", "")
+AWS_S3_ENDPOINT_URL = env.get_str("AWS_S3_ENDPOINT_URL", "")
 AWS_S3_REGION_NAME = env.get_str("AWS_S3_REGION_NAME", "")
 # AWS_S3_CUSTOM_DOMAIN = env.get_str(
 #     "AWS_S3_CUSTOM_DOMAIN", ""
