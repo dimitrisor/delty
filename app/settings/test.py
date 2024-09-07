0a9bbff9104201aa6dcc59d723c07bd11648ded5
dimitris orfanoudakis <dimitrisorfanuakis@gmail.com>
2024-09-07 13:43:26 +0300

    feat: Setup CI and CD workflows

--- app/settings/test.py
+++ app/settings/test.py
@@ -1,16 +1,8 @@
-import dj_database_url
-
 ENVIRONMENT = "test"
 from app.settings.base import *  # noqa
 
 DEBUG = False
 STATICFILES_STORAGE = ""
-DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://delty_user:delty_pass@localhost:5432/delty",
-        conn_max_age=600,
-    )
-}
 
 DRAMATIQ_BROKER = {
     "BROKER": "dramatiq.brokers.stub.StubBroker",
