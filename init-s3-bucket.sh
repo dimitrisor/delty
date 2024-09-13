ad84a2e3f5e54ac69a9afed095fd48baf7d5dde4
dimitris orfanoudakis <dimitrisorfanuakis@gmail.com>
2024-09-13 15:18:53 +0300

    feat: Replace localstack with minio

--- init-s3-bucket.sh
+++ init-s3-bucket.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Create S3 Bucket..."
-awslocal s3 mb s3://selected-element-html-s3-bucket
+# Wait until MinIO server is up
+until $(curl --output /dev/null --silent --head --fail http://localhost:9000); do
+  echo "Waiting for MinIO server to start..."
+  sleep 3
+done
 
-echo "List S3 Buckets..."
-awslocal s3 ls
+# Set up the MinIO alias and create the bucket
+mc alias set myminio http://localhost:9000 minioadmin minioadmin
+mc mb myminio/selected-element-html-s3-bucket
